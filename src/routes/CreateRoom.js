456f9b369c75d28d211ad18def12e3e1f89a451f
Lukas Seklir <lukas.seklir@gmail.com>
2021-02-12 14:43:00 -0800

    feat(room generation): rooms now generate with a 6 digit all uppercase letter code

--- src/routes/CreateRoom.js
+++ src/routes/CreateRoom.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { v1 as uuid } from "uuid";
+
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
 
 const CreateRoom = (props) => {
     function create() {
-        const id = uuid();
+        const id = makeid(6);
         props.history.push(`/room/${id}`);
     }
 
