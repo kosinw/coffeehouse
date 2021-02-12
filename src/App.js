ea5997877cafdbea8a6436d4bfecd422ec1d02f1
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-02-12 11:51:39 -0800

    feat(video): added login and callpage to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import LoginPage from "./components/LoginPage";
+import CallPage from "./components/CallPage";
+
 function App() {
   return (
     <div className="App">
       hackhw21
+      <LoginPage/>
+      <CallPage/>
     </div>
   );
 }
---
+++ src/components/LoginPage.js
@@ -0,0 +1,19 @@
+function LoginPage() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h2>WebRTC Video Demo. Please sign in</h2>
+                    <label>Login</label>
+                    <input>
+                        <button>
+                            Sign in
+                        </button>
+                    </input>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
