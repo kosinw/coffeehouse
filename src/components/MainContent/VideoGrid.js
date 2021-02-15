6c44c000d2bcc79e5184afae5bea5ad1ddc6ae0c
Kosi Nwabueze <kosinwabueze@gmail.com>
2021-02-14 20:15:48 -0800

    fix(socket): reading env var for dev

--- src/components/MainContent/VideoGrid.js
+++ src/components/MainContent/VideoGrid.js
@@ -95,7 +95,7 @@ function VideoGrid({ userVideo }) {
     });
 
     useEffect(() => {
-        assignRef(socketRef, io.connect(process.env.REACT_APP_HOST_SERVER || "52.25.140.5:8000"));
+        assignRef(socketRef, io.connect(process.env.REACT_APP_HOST_SERVER || "https://infinite-ravine-45588.herokuapp.com/"));
         setSocketInitialized(true);
 
         navigator.mediaDevices.getUserMedia(constraints).then(stream => {
